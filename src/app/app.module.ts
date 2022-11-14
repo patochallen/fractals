50fcc9d93b4c5ee824873c5d53ad6f063239e08c
patochallen <patofch@gmail.com>
2022-11-13 21:11:48 -0300

    Create fractal component #5 fractal component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FractalComponent } from './fractal/fractal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FractalComponent
   ],
   imports: [
     BrowserModule
