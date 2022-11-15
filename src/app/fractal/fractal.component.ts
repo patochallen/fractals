02dc92f5fe585c28634d195c705b313cf3cb8a85
patochallen <patofch@gmail.com>
2022-11-15 00:54:06 -0300

    Clean up fractal component and remove styles #17 fractal component cleaned

--- src/app/fractal/fractal.component.ts
+++ src/app/fractal/fractal.component.ts
@@ -1,34 +1,36 @@
-import { Fractal } from './../core/model/fractal';
-import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { FractalService } from './../core/service/fractal.service';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fractal',
-  templateUrl: './fractal.component.html',
-  styleUrls: ['./fractal.component.scss']
+  templateUrl: './fractal.component.html'
 })
-export class FractalComponent implements AfterViewInit {
-  @ViewChild('canvas')
-  private canvas: ElementRef | null = null;
-
-  @Input() lineWidth: number = 10;
-  @Input() sides: number = 3;
-  @Input() scale: number = 0.5;
-  @Input() spread: number = 1;
-  @Input() color: string = 'hsl(' + Math.random()*360 + ', 100%, 50%)';
+export class FractalComponent implements OnInit, AfterViewInit {
 
   private context: CanvasRenderingContext2D | null = null;
+
+  private fractal$ = this.fractalService.getFractal();
 
   public width: number = window.innerWidth;
 
   public height: number = window.innerHeight;
 
-  constructor() { }
-
-  ngAfterViewInit(): void {
-    this.context = this.canvas?.nativeElement.transferControlToOffscreen().getContext('2d');
-    if (this.context) {
-      new Fractal(this.width, this.height).draw(this.context)
-    }
+  constructor(
+    private fractalService: FractalService,
+    private elementRef: ElementRef
+  ) {
+    this.fractal$.subscribe(fractal => {
+      if (fractal && this.context) fractal.draw(this.context)
+    })
   }
 
+  ngOnInit(): void {
+    this.width = this.elementRef?.nativeElement?.offsetWidth;
+    this.height = this.elementRef?.nativeElement?.offsetHeight;
+  }
+
+  ngAfterViewInit(): void {
+    this.context = this.elementRef?.nativeElement?.firstChild?.transferControlToOffscreen()?.getContext('2d');
+    this.fractalService.createFractal(this.width, this.height);
+  }
 }
