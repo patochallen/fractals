c2518829ddb5213e41d1376e0787c661d81fcd53
Patricio Challen <37918735+patochallen@users.noreply.github.com>
2022-11-15 01:48:24 -0300

    Create controls component to change fractal state #19 (#20)
    controls component created

---
+++ src/app/fractal/controls/controls.component.ts
@@ -0,0 +1,75 @@
+import { FractalService } from './../../core/service/fractal.service';
+import { map } from 'rxjs';
+import { FractalState } from './../../core/model/fractal-state';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controls',
+  templateUrl: './controls.component.html',
+  styleUrls: ['./controls.component.scss']
+})
+export class ControlsComponent {
+
+  public state$ = this.fractalService.getFractal().pipe(
+    map(fractal => <FractalState> {
+      lineWidth: fractal?.lineWidth ?? 1,
+      sides: fractal?.sides ?? 3,
+      scale: fractal?.scale ?? 0.5,
+      angle: Math.floor((fractal?.angle ?? 1) * 180 / Math.PI),
+      deept: fractal?.maxDeept ?? 4,
+      branches: fractal?.branches ?? 2,
+      color: fractal?.color ?? 'gold',
+    })
+  );
+
+  constructor(
+    private fractalService: FractalService
+  ) { }
+
+  randomize(): void {
+    this.fractalService.generateRandomFractal();
+  }
+
+  setSides(target: EventTarget | null): void {
+    let newValue = Number((target as HTMLInputElement).value);
+    if (newValue) {
+      this.fractalService.setSides(newValue);
+    }
+  }
+
+  setLineWidth(target: EventTarget | null): void {
+    let newValue = Number((target as HTMLInputElement).value);
+    if (newValue) {
+      this.fractalService.setLineWidth(newValue);
+    }
+  }
+
+  setScale(target: EventTarget | null): void {
+    let newValue = Number((target as HTMLInputElement).value);
+    if (newValue) {
+      this.fractalService.setScale(newValue);
+    }
+  }
+
+  setAngle(target: EventTarget | null): void {
+    let newValue = Number((target as HTMLInputElement).value);
+    if (newValue) {
+      console.log(newValue, newValue * Math.PI / 180);
+      this.fractalService.setAngle((newValue - 1) * Math.PI / 180);
+    }
+  }
+
+  setMaxDeept(target: EventTarget | null): void {
+    let newValue = Number((target as HTMLInputElement).value);
+    if (newValue) {
+      this.fractalService.setMaxDeept(newValue);
+    }
+  }
+
+  setBranches(target: EventTarget | null): void {
+    let newValue = Number((target as HTMLInputElement).value);
+    if (newValue) {
+      this.fractalService.setBranches(newValue);
+    }
+  }
+}
