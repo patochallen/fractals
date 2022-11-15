de16ef2645b1ad40265f9f32d4b1663e9af36f0a
patochallen <patofch@gmail.com>
2022-11-15 05:20:03 -0300

    Set initial values for fractals #23 initial values setted

--- src/app/core/service/fractal.service.ts
+++ src/app/core/service/fractal.service.ts
@@ -22,10 +22,10 @@ export class FractalService {
   generateRandomFractal(): void {
     let fractal = this.fractal$.value;
     if (fractal) {
-      fractal.lineWidth = Math.floor(Math.random() * 50 + 1);
-      fractal.sides = Math.floor(Math.random() * 9 + 1);
-      fractal.scale = (Math.random() * 0.2 + 0.4);
-      fractal.angle = (Math.random() * 3.1 + 0);
+      fractal.lineWidth = Math.floor(Math.random() * 40 + 10);
+      fractal.sides = Math.floor(Math.random() * 7 + 3);
+      fractal.scale = Math.random() * 0.2 + 0.4;
+      fractal.angle = Math.random() * 2 + 0.5;
       fractal.color = 'hsl(' + Math.floor(Math.random() * 360) + ', 100%, 50%)';
       fractal.maxDeept = 4;
       fractal.branches = 2;
